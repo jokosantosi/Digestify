7f811b2aeb5e7e685dde9d054f2fbe2c151f6470
PramudyaReynaldi <prmdy02@gmail.com>
2024-05-13 15:14:16 +0700

    setup daisyui & add navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/partials/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -13,6 +16,16 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    container: {
+      center: true,
+      padding: "1.25rem",
+      screens: {
+        xl: "1200px",
+        "2xl": "1200px",
+      }
+    }
   },
-  plugins: [],
+  plugins: [
+    daisyui,
+  ],
 };
