737acc3806bf2f6ff33da6316199d01c3386ab34
PramudyaReynaldi <prmdy02@gmail.com>
2024-05-18 16:25:46 +0700

    move libs folder to modul

--- src/components/CardNews/index.jsx
+++ src/components/CardNews/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import axiosSession from "@/libs/axiosSession";
+import axiosSession from "@/modules/axiosSession";
 import { Card, Col, Row } from "react-bootstrap";
-import { FaHeart } from "react-icons/fa";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 import Link from "next/link";
 
 const CardNews = () => {
@@ -25,6 +25,21 @@ const CardNews = () => {
         } 
     };
 
+    // Create initial likes state
+    const initialLikesState = news.reduce((acc, item) => {
+        acc[item.id] = false;
+        return acc;
+    }, {});
+
+    const [like, setLike] = useState(initialLikesState);
+
+    const handleLike = (id) => {
+        setLike((prevLikes) => ({
+          ...prevLikes,
+          [id]: !prevLikes[id],
+        }));
+    };
+
     useEffect(() => {
         fetchData();
     }, []);
@@ -39,7 +54,11 @@ const CardNews = () => {
                             <Card.Body className="px-0">
                                 <Card.Title className="d-flex justify-content-between">
                                     <h5 style={{ width: '94%'}}>{item.title}</h5>
-                                    <FaHeart />
+                                    {like[item.id] ? (
+                                        <FaHeart style={{ cursor: 'pointer' }} onClick={() => handleLike(item.id)} />
+                                    ) : (
+                                        <FaRegHeart style={{ cursor: 'pointer' }} onClick={() => handleLike(item.id)} />
+                                    )}
                                 </Card.Title>
                                 <div className="d-flex justify-content-between align-items-center mb-2">
                                     <div className="d-flex gap-3 align-items-center">
@@ -49,8 +68,8 @@ const CardNews = () => {
                                             </Card.Subtitle>
                                         ))}
                                     </div>
-                                    <Link href={item.original_url} target="_blank">
-                                        <Card.Subtitle className="bg-item px-3 py-1 rounded-3">
+                                    <Link href={item.original_url} target="_blank" className="text-decoration-none">
+                                        <Card.Subtitle className="bg-item px-3 py-1 rounded-3 text-secondary">
                                             Read More
                                         </Card.Subtitle>
                                     </Link>
